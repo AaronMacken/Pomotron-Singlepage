7946e666dda60fa2e8a5c0ed7ea442edda5c1433
Aaron Macken <aaron.a.macken@gmail.com>
2019-07-19 23:24:18 -0700

    Add remaining functionality to the delete request for temporary and persisting completed objects.

--- public/js/ajax.js
+++ public/js/ajax.js
@@ -24,18 +24,28 @@ $(document).ready(function() {
       .remove();
   });
 
-  // remove current tasks --------------------------------------------------- ONLY WORKS ON PERMANENT OBJECTS, TEMPORARY CLOSE WILL NOT PERSIST -------------------------------------------------
+//   current task remove
   $(".list").on("click", ".delete", function(e) {
     e.stopPropagation();
     removeTask($(this).parent());
   });
 
-  // remove completed tasks
+  // completed task remove
+//   this list has several remove functions because the displayed object are either
+//   A) Permanent items from the database that persist on refresh
+//   B) Copies of list items that are temporarily stored in memory - do not persist
+//   The first function removes the items that persist, same as above
+//   The second function makes an ajax delete request with based off of an ID value that is
+//   passed to the temporary list item object. This ID comes from the database's returned ID and is
+//   then used to add onto the URL of the ajax end point 
   $(".list2").on("click", ".delete", function(e) {
     e.stopPropagation();
+    // console.log($(this).parent().attr('id'));
     removeTask($(this).parent());
-    $(this).parent().remove();
-    console.log($(this).parent());
+    removeTemp($(this).parent().attr('id'));
+    $(this)
+      .parent()
+      .remove();
   });
 });
 
@@ -67,7 +77,6 @@ function loadTask(task) {
       "</li>"
   );
 
-  
   // jquery memory variables
   newTask.data("id", task._id);
   newTask.data("inProgress", task.inProgress);
@@ -115,18 +124,17 @@ function moveTask(task) {
   let updateUrl = "/api/tasks/" + task.data("id");
   let switchVar = !task.data("isCompleted");
   let updateData = { isCompleted: switchVar };
+  let taskId = task.data("id");
   $.ajax({
     method: "PUT",
     url: updateUrl,
     data: updateData
   }).then(function(updatedTask) {
     task.data("isCompleted", switchVar);
-
     // Create a temporary object to be stored on the complted task list
-
     if (updatedTask.isCompleted == true) {
       let newTask = $(
-        "<li class='task'>" +
+        "<li class='task' >" +
           updatedTask.name +
           '<button class="btn btn--ajax delete"><i class="fas fa-times"></i></button>' +
           '<ul class="nestedList"><li>Expected iterations: ' +
@@ -135,11 +143,15 @@ function moveTask(task) {
           "</li>" +
           "<li>Completed iterations: " +
           updatedTask.completedIterations +
+          +taskId +
           "</li></ul>" +
           "</li>"
       );
       $(".list2").append(newTask);
       // temporarily remove from main list done on the button's on click
+      // give this new temporary task li object an id equal to the db objects ID
+      newTask.attr("id", taskId);
+      //   console.log(newTask.attr('id'));
     }
   });
 }
@@ -147,6 +159,17 @@ function moveTask(task) {
 // remove an existing db object
 function removeTask(task) {
   let deleteUrl = "/api/tasks/" + task.data("id");
+  $.ajax({
+    method: "DELETE",
+    url: deleteUrl
+  }).then(function(data) {
+    task.remove();
+    console.log(data);
+  });
+}
+
+function removeTemp(tempId) {
+  let deleteUrl = "/api/tasks/" + tempId;
   $.ajax({
     method: "DELETE",
     url: deleteUrl
